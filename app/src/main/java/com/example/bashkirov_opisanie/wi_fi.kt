e90a9ad845902e38329c51042391b242909b0ac2
Vyacheslav <94893819+6xisp1@users.noreply.github.com>
2022-05-20 14:28:57 +0700

    proekt_20.05

--- app/src/main/java/com/example/bashkirov_opisanie/wi_fi.kt
+++ app/src/main/java/com/example/bashkirov_opisanie/wi_fi.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class wi_fi : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_wi_fi)
+        setContentView(R.layout.wi_fi)
     }
 }
