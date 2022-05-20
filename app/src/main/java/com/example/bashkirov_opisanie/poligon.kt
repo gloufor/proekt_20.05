0f1195ba17eff3985ddd198a9a4fa591767daa6f
Vyacheslav <94893819+6xisp1@users.noreply.github.com>
2022-05-20 08:45:39 +0700

    proekt_20.05

---
+++ app/src/main/java/com/example/bashkirov_opisanie/poligon.kt
@@ -0,0 +1,11 @@
+package com.example.bashkirov_opisanie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class poligon : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_poligon)
+    }
+}
